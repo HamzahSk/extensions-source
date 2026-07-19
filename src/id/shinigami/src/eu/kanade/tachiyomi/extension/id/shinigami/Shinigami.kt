472bccf66bd4a766666f15f3286b3fd9616cf397
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-19 20:28:00 +0700

    Last Sync: 2026-07-19 20:27 (Mobile)

--- src/id/shinigami/src/eu/kanade/tachiyomi/extension/id/shinigami/Shinigami.kt
+++ src/id/shinigami/src/eu/kanade/tachiyomi/extension/id/shinigami/Shinigami.kt
@@ -97,6 +97,8 @@ abstract class Shinigami : HttpSource(), ConfigurableSource {
     }
 
     override fun latestUpdatesParse(response: Response): MangasPage = popularMangaParse(response)
+    
+    override fun recomendationsParse(response: Response): MangasPage = popularMangaParse(response)
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = "$apiUrl/v1/manga/list".toHttpUrl().newBuilder()
